3ea4588ea0dc237306b50433f6497603e7f9448b
AirProDiag <140640309+AirProDiag@users.noreply.github.com>
2024-07-19 11:06:41 -0400

    Update disable_updates.sh

--- disable_updates.sh
+++ disable_updates.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
- 
+
 # Define the configuration file
 CONFIG_FILE="/etc/apt/apt.conf.d/20auto-upgrades"
- 
+
 # Set the desired value
 NEW_VALUE="0"
- 
+
 # Update the configuration file
-sudo sed -i "s/\(^\s*APT::Periodic::Unattended-Upgrade\s*\).*\$/\1\"$NEW_VALUE\";/" "$CONFIG_FILE"
- 
+sudo sed -i 's/\(^\s*APT::Periodic::Unattended-Upgrade\s*\).*\$/\1\"$NEW_VALUE\";/' '$CONFIG_FILE'
+
 # Restart the unattended-upgrades service
 sudo systemctl restart unattended-upgrades
- 
+
 echo "APT::Periodic::Unattended-Upgrade set to $NEW_VALUE"
